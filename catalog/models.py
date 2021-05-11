07d078cb1e928c7d32a8d9bd4e1f861446376e0d
Baye Dev <sowsoule@gmail.com>
2021-05-12 02:45:38 +0300

    first commit only admin

---
+++ catalog/models.py
@@ -0,0 +1,95 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+
+
+class Genre(models.Model):
+    """Model representing a genre book """
+    name =  models.CharField(max_length=200, help_text='Enter book genre(e.g. Science Fiction)')
+
+    def __str__(self):
+        """String representing the Model object"""
+        return self.name
+
+class Language(models.Model):
+    """Model representing a Language (English, French, etc"""
+    name = models.CharField(max_length=100, help_text='Enter the books natural language')
+
+    def __str__(self):
+        """Printing the model representation"""
+        return self.name
+
+class Book(models.Model):
+    """Model representing a book but not a very specific book"""
+
+    title = models.CharField(max_length=200)
+    # Foreign key used because book can only have one author, but authors can have multiple books"""
+    # Author as sa string rather than an object because it has not been declared in the file yet
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13, unique=True)
+
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    genre = models.ManyToManyField(Genre, help_text='Select genre for this book')
+
+    def __str__(self):
+        """String representing the model Model object"""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book"""
+        return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """Create a string for Genre. This is required to display genre in Admin"""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre'
+
+class BookInstance(models.Model):
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this paritcular book across the whole library ')
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id}({self.book.title})'
+
+class Author(models.Model):
+    """Model representing the author"""
+    first_name = models.CharField(max_length=100)
+    last_name  = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance"""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """string representing the model object"""
+        return f'{self.first_name}, {self.last_name}'
